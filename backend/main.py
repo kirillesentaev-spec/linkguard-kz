ce6ccb2e19f2006b28947d2acd10fb7b1b2d24a7
Nurkerei <kirillesentaev@gmail.com>
2026-09-25 04:36:29 +0600

    Allow production frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -21,10 +21,11 @@ init_database()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+   allow_origins=[
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://linkguard-kz-1.onrender.com"
+],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
